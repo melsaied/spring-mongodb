0000fd9bcf17718cf69a50dc7dea032f7ad0cded
First Last <email.mail.com>
2020-05-10 05:24:05 -0500

    Changing "cloudtech"

--- src/main/java/com/CloudTech/springmongodb/Application.java
+++ src/main/java/com/CloudTech/springmongodb/Application.java
@@ -1,4 +1,4 @@
-package com.CloudTech.springmongodb;
+package com.cloudtech.springmongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
