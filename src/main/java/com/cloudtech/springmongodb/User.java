900c49792be4f1244b2168dc9a84f16d4c5d5ebd
First Last <email.mail.com>
2021-04-25 23:35:37 -0500

    Adding new DB URL

--- src/main/java/com/cloudtech/springmongodb/User.java
+++ src/main/java/com/cloudtech/springmongodb/User.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collection = "Users")
+@Document(collection = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
